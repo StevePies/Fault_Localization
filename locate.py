1a2c6966ea7559bbe6aee09044fc04961d5f5b18
Jenson <jingyangyang@outlook.com>
2019-11-18 13:05:56 +0800

    fix

--- locate.py
+++ locate.py
@@ -10,22 +10,21 @@ class Locate:
         self._end = _end
         self._kpi = _kpi
        
-
-    def getDataFromES():
+    def getDataFromES(self):
         self.es_data = es_load.search(self._start,self._end,self._kpi)
         for item in self.es_data:
             print(item)
         
-    def groupby_3d():
+    def groupby_3d(self):
         i=0
         
-    def iswift():
+    def iswift(self):
         i=0
 
 
-    def insert_to_db():
+    def insert_to_db(self):
         i=0
 
 if __name__ == "__main__":
-    locat = Locate(1,1,1,20190610083800,20190610183800,"OUT__FLOW")
-    locat.getDataFromES()
+    locate = Locate(1,1,1,20190610083800,20190610183800,"OUT__FLOW")
+    locate.getDataFromES()
