69924f0f153af143b2de231b1545ecde6e1af92b
Jenson <jingyangyang@outlook.com>
2019-11-29 09:17:02 +0800

    commit

--- src/post.py
+++ src/post.py
@@ -12,8 +12,8 @@ data={
 	"kpi": "OUT_FLOW"
 }
 print(data)
-url="http://127.0.0.1:8383/locate"
-#url="http://10.10.27.130:8383/locate"
+#url="http://127.0.0.1:8383/locate"
+url="http://10.10.27.130:8383/locate"
 data_json = json.dumps(data)
 headers = {'Content-type': 'application/json'}
 response = requests.post(url, data=data_json, headers=headers)
