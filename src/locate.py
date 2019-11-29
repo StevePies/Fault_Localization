d42ffe11e7d04b0f01e2ef030235b0984e2cf234
Jenson <jingyangyang@outlook.com>
2019-11-29 16:16:52 +0800

    cmt

--- src/locate.py
+++ src/locate.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 import pandas as pd
-import itertools,math,sys,datetime,re
+import itertools,math,sys,datetime
 from model.iswift import iswift 
 from util.db_util import MysqldbHelper
 import util.es_load 
@@ -26,10 +26,8 @@ class Locate:
         self.db=MysqldbHelper()   
         sql = "insert into rca_task_table (rcaId,type,name,startTime,endTime,kpi,model,createtime,state) values ('"+\
             self._task_id+"','"+self._type+"','"+self._name+"','"+self._start+"','"+self._end+"','"+self._kpi+"','"+self._model+"','"+self.create_time+"','"+str(0)+"')"
-        regex = re.compile(r'\\(?![/u"])')
-        fixed_sql = regex.sub(r"\\\\", sql)
-        self.db.update(fixed_sql)
-        print(fixed_sql)
+        self.db.update(sql)
+        print(sql)
 
     def getDataFromES(self):
         self.list = []
@@ -57,10 +55,8 @@ class Locate:
         print("get data from es successful!")
 
         sql = "UPDATE rca_task_table SET state = '1' WHERE rcaId = '"+self._task_id+"'"
-        regex = re.compile(r'\\(?![/u"])')
-        fixed_sql = regex.sub(r"\\\\", sql)
-        self.db.update(fixed_sql)
-        print(fixed_sql)
+        self.db.update(sql)
+        print(sql)
        
     def dimCombination(self,dim_arr,i):
         result = []
@@ -117,25 +113,22 @@ class Locate:
         #print(self.d3_tree[0])
         print("groupby finished!")
         sql = "UPDATE rca_task_table SET state = '2' WHERE rcaId = '"+self._task_id+"'"
-        regex = re.compile(r'\\(?![/u"])')
-        fixed_sql = regex.sub(r"\\\\", sql)
-        self.db.update(fixed_sql)
-        print(fixed_sql)
+        self.db.update(sql)
+        print(sql)
                     
     def algorithm(self):
         #TODO
         
         ift = iswift(self.d3_tree,self.list)
         sql = "UPDATE rca_task_table SET state = '3' WHERE rcaId = '"+self._task_id+"'"
-        regex = re.compile(r'\\(?![/u"])')
-        fixed_sql = regex.sub(r"\\\\", sql)
-        self.db.update(fixed_sql)
-        print(fixed_sql)
-        self.result = ift.run()
+        self.db.update(sql)
+        print(sql)
+        rt = ift.run()
+        import MySQLdb
+        self.result = MySQLdb.escape_string(rt)
+
         self.over_time=datetime.datetime.now().strftime('%Y%m%d%H%M%S')
         sql = "UPDATE rca_task_table SET state = '9',overTime = '"+self.over_time+"',result = '"+str(self.result)+"' WHERE rcaId = '"+self._task_id+"'"
-        regex = re.compile(r'\\(?![/u"])')
-        fixed_sql = regex.sub(r"\\\\", sql)
-        self.db.update(fixed_sql)
-        print(fixed_sql)
+        self.db.update(sql)
+        print(sql)
 
