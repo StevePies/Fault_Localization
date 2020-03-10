a52aebc4437988a57bf4967d93356d4e2fca0bc8
Jenson QAQ <Jenson@jensondeMacBook-Pro.local>
2020-03-10 11:46:31 +0800

    tt

--- src/post.py
+++ src/post.py
@@ -12,9 +12,12 @@ data={
 	"kpi": "OUT_FLOW",
 	"remarks":"test remarks"
 }
-print(data)
-url="http://127.0.0.1:8383/api/rcaService"
-data_json = json.dumps(data)
-headers = {'Content-type': 'application/json'}
-response = requests.post(url, data=data_json, headers=headers)
-print(response.text)
+for i in range(0,30):
+	data['racId'] = str(int(data['racId'])+1)
+
+	print(data)
+	url="http://127.0.0.1:8383/api/rcaService"
+	data_json = json.dumps(data)
+	headers = {'Content-type': 'application/json'}
+	response = requests.post(url, data=data_json, headers=headers)
+	print(response.text)
